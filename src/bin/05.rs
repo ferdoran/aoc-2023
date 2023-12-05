8fcd6622080d2d57aaeaa38afdf102496caf313e
Roland-Georg Müller <roland-georg.mueller@rtl.de>
2023-12-05 13:21:49 +0100

    improve solution for day 5

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -4,18 +4,41 @@ advent_of_code::solution!(5);
 
 
 pub struct SeedMaps(Vec<SeedMap>);
+
 impl SeedMaps {
     pub fn map(&self, seed: u64) -> u64 {
         let mut seed = seed;
+        // let map = self.0[0];
         for map in &self.0 {
             if let Some(v) = map.map(seed) {
-                seed = v
+                seed = v;
             }
         }
 
         seed
     }
+
+    pub fn rev_map(&self, location: u64) -> u64 {
+        let mut location = location;
+        for map in self.0.iter().rev() {
+            if let Some(v) = map.rev_map(location) {
+                location = v
+            }
+        }
+        location
+    }
+
+    pub fn get_min_location(&self) -> u64 {
+        let location_ranges = self.0.iter().last().unwrap();
+        location_ranges.0.iter().map(|loc_range| {
+            loc_range.dest_range.start
+        })
+            .min()
+            .map(|loc| self.rev_map(loc))
+            .unwrap()
+    }
 }
+
 pub struct SeedMap(pub Vec<SeedRange>);
 
 impl SeedMap {
@@ -24,12 +47,18 @@ impl SeedMap {
             .filter_map(|r| r.map(input))
             .next()
     }
+
+    pub fn rev_map(&self, input: u64) -> Option<u64> {
+        self.0.iter().rev()
+            .filter_map(|r| r.rev_map(input))
+            .next()
+    }
 }
 
 #[derive(Debug)]
 pub struct SeedRange {
     pub source_range: Range<u64>,
-    pub dest_range: Range<u64>
+    pub dest_range: Range<u64>,
 }
 
 impl SeedRange {
@@ -38,15 +67,22 @@ impl SeedRange {
             let offset = input - self.source_range.start;
             return Some(self.dest_range.start + offset);
         }
-        return None
+        return None;
+    }
+    pub fn rev_map(&self, input: u64) -> Option<u64> {
+        if self.dest_range.contains(&input) {
+            let offset = input - self.dest_range.start;
+            return Some(self.source_range.start + offset);
+        }
+        return None;
     }
 }
-
 
 
 pub fn part_one(input: &str) -> Option<u64> {
     let (mut lines, seeds) = parse_seeds(input);
     let maps = parse_maps(&mut lines);
+    // let final_map = maps.to_final_map();
 
     let min_location = seeds.iter().map(|seed| maps.map(*seed)).min().unwrap();
 
@@ -96,7 +132,7 @@ fn parse_maps(lines: &mut Lines) -> SeedMaps {
                 let dest_range = dest_start..dest_start + len;
                 let seed_range = SeedRange {
                     source_range,
-                    dest_range
+                    dest_range,
                 };
                 current_map.push(seed_range)
             }
@@ -118,7 +154,7 @@ fn parse_maps(lines: &mut Lines) -> SeedMaps {
         water_to_light,
         light_to_temp,
         temp_to_humid,
-        humid_to_location
+        humid_to_location,
     ];
 
     let maps = SeedMaps(maps);
@@ -128,18 +164,44 @@ fn parse_maps(lines: &mut Lines) -> SeedMaps {
 pub fn part_two(input: &str) -> Option<u64> {
     let (mut lines, seeds) = parse_seeds(input);
     let maps = parse_maps(&mut lines);
-
     let seeds: Vec<Range<u64>> = seeds.chunks(2).map(|range| {
-        range[0]..range[0]+range[1]
+        range[0]..range[0] + range[1]
     }).collect();
 
-    let min_location = seeds.iter().filter_map(|range| {
-        range.clone().map(|seed| maps.map(seed)).min()
-    })
-        .min()
-        .unwrap();
+    let min_loc = maps.get_min_location();
 
-    Some(min_location)
+    return if seeds.iter().any(|r| r.contains(&min_loc)) {
+        Some(min_loc)
+    } else {
+        println!("min loc {} not in seeds", min_loc);
+        let mut min_loc = min_loc;
+        let mut range_step = 256;
+        let mut min = min_loc;
+
+        let mut min_start = 0;
+        while range_step != 0 {
+            println!("range_step: {}", range_step);
+            range_step /= 2;
+            if range_step == 0 {
+                break
+            }
+            for l in (min_start..min_loc).step_by(range_step as usize) {
+                let s = maps.rev_map(l);
+                if seeds.iter().any(|r| r.contains(&s)) {
+                    if l <= min {
+                        println!("found new min_loc: {l}");
+                        min = l;
+                        min_loc = l;
+                        min_start = l - 2 * range_step;
+                    } else {
+                        break
+                    }
+                }
+            }
+            range_step /= 2;
+        }
+        Some(min)
+    }
 }
 
 #[cfg(test)]
